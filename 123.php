cc228b07ed2dff02bd100ee77725091fde87ddde
Administrator <1033383881@qq.com>
2020-05-19 14:08:21 +0800

    Initial commit

---
+++ 123.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 1368834663
+ * Date: 2019/3/8
+ * Time: 16:24
+ */
+for($i=1;$i<=9;$i++){
+    for($j=1;$j<=$i;$j++){
+        echo $i."*".$j."=".$i*$j."  ";
+    }
+    echo "<br>";
+}
